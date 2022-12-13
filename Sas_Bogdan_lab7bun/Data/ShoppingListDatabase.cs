535564cfa745d907b8f4ccd4929237f7dafd1380
Bogdii13 <bogdanionutsas13@gmail.com>
2022-12-13 15:38:16 +0200

    lab8 bun

--- Sas_Bogdan_lab7bun/Data/ShoppingListDatabase.cs
+++ Sas_Bogdan_lab7bun/Data/ShoppingListDatabase.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using System.Threading.Tasks;
+using Sas_Bogdan_lab7bun.Models;
 
 namespace Sas_Bogdan_lab7bun.Data
 {
-    class ShoppingListDatabase
+    public class ShoppingListDatabase
     {
+        readonly SQLiteAsyncConnection _database;
+        public ShoppingListDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<ShopList>().Wait();
+        }
+        public Task<List<ShopList>> GetShopListsAsync()
+        {
+            return _database.Table<ShopList>().ToListAsync();
+        }
+        public Task<ShopList> GetShopListAsync(int id)
+        {
+            return _database.Table<ShopList>()
+            .Where(i => i.ID == id)
+           .FirstOrDefaultAsync();
+        }
+        public Task<int> SaveShopListAsync(ShopList slist)
+        {
+            if (slist.ID != 0)
+            {
+                return _database.UpdateAsync(slist);
+            }
+            else
+            {
+                return _database.InsertAsync(slist);
+            }
+        }
+        public Task<int> DeleteShopListAsync(ShopList slist)
+        {
+            return _database.DeleteAsync(slist);
+        }
     }
 }
--- Sas_Bogdan_lab7bun/Models/ShopList.cs
+++ Sas_Bogdan_lab7bun/Models/ShopList.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace Sas_Bogdan_lab7bun.Models
 {
     class ShopList
     {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
     }
 }
