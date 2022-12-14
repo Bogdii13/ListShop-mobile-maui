6b51468e2c39dcf373a6efc8a136def86d79514a
Bogdii13 <bogdanionutsas13@gmail.com>
2022-12-14 14:55:54 +0200

    laborator 10

--- Sas_Bogdan_lab7bun/ListPage.xaml.cs
+++ Sas_Bogdan_lab7bun/ListPage.xaml.cs
@@ -11,6 +11,8 @@ public partial class ListPage : ContentPage
     {
         var slist = (ShopList)BindingContext;
         slist.Date = DateTime.UtcNow;
+        Shop selectedShop = (ShopPicker.SelectedItem as Shop);
+        slist.ShopID = selectedShop.ID;
         await App.Database.SaveShopListAsync(slist);
         await Navigation.PopAsync();
     }
@@ -32,6 +34,9 @@ public partial class ListPage : ContentPage
     protected override async void OnAppearing()
     {
         base.OnAppearing();
+        var items = await App.Database.GetShopsAsync();
+        ShopPicker.ItemsSource = (System.Collections.IList)items;
+        ShopPicker.ItemDisplayBinding = new Binding("ShopDetails");
         var shopl = (ShopList)BindingContext;
 
         listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
--- Sas_Bogdan_lab7bun/ShopPage.xaml.cs
+++ Sas_Bogdan_lab7bun/ShopPage.xaml.cs
@@ -1,4 +1,6 @@
 namespace Sas_Bogdan_lab7bun;
+
+using Plugin.LocalNotification;
 using Sas_Bogdan_lab7bun.Models;
 
 public partial class ShopPage : ContentPage
@@ -27,6 +29,22 @@ public partial class ShopPage : ContentPage
         var location = locations?.FirstOrDefault();
         // var myLocation = await Geolocation.GetLocationAsync();
         var myLocation = new Location(46.7731796289, 23.6213886738);
+        var distance = myLocation.CalculateDistance(location,
+DistanceUnits.Kilometers);
+        if (distance < 4)
+        {
+            var request = new NotificationRequest
+            {
+                Title = "Ai de facut cumparaturi in apropiere!",
+                Description = address,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(1)
+                }
+            };
+            LocalNotificationCenter.Current.Show(request);
+        }
+
         await Map.OpenAsync(location, options);
     }
 }
